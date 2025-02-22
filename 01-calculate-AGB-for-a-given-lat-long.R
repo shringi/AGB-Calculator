7dbbd178a4ccb23563c414740e49ea632efb724e
shringi <ankurshringi@iisc.ac.in>
2025-02-22 05:51:35 +0530

    Created 01-calculate-AGB-for-a-given-lat-long.R

---
+++ 01-calculate-AGB-for-a-given-lat-long.R
@@ -0,0 +1,5 @@
+# Author: Ankur Shringi (ankurshringi@iisc.ac.in)
+# title: 01-calculate-AGB-for-a-given-lat-long.R
+# abstract: Given a latitude and longitude, plot relationship between AGB and D.
+# Project: AGB-Calculator
+# Date created: 2025-Feb-22 05:49:19 Saturday
